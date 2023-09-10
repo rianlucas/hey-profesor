79c0acfa34de836bbf1179ba5c1e1ba8fef4b9da
Rian Lucas <rianluccas08@gmail.com>
2023-09-10 17:02:18 -0300

    HEY-6: wip

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\{ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
--- tests/Feature/CreateAQuestionsTest.php
+++ tests/Feature/CreateAQuestionsTest.php
@@ -1,17 +1,15 @@
 <?php
 
 use App\Models\User;
-use function Pest\Laravel\actingAs;
-use function Pest\Laravel\assertDatabaseCount;
-use function Pest\Laravel\assertDatabaseHas;
-use function Pest\Laravel\post;
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
 
 it('should be able to create a question bigger than 255 characters', function () {
     $user = User::factory()->create();
     actingAs($user);
 
     $request = post(route('question.store'), [
-       'question' => str_repeat('*', 260). '?',
+        'question' => str_repeat('*', 260) . '?',
     ]);
 
     $request->assertRedirect(route('dashboard'));
@@ -23,6 +21,6 @@ it('should check if end with question mark ?', function () {
 
 });
 
-it('should have at least 10 character', function() {
+it('should have at least 10 character', function () {
 
 });
