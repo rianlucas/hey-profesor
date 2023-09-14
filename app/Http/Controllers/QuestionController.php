4e2a371dd987480d989cefcd3d3d312c88b5cb94
Rian Lucas <rianluccas08@gmail.com>
2023-09-14 18:43:41 -0300

    HEY-6: refactor create a rule to check if end with question mark

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Closure;
+use App\Rules\EndWithQuestionMarkRule;
 use Illuminate\Http\{RedirectResponse, Request, Response};
 
 class QuestionController extends Controller
@@ -14,11 +14,8 @@ class QuestionController extends Controller
             'question' => [
                 'required',
                 'min:10',
-                function (string $attribute, mixed $value, Closure $fail) {
-                    if ($value[strlen($value) - 1] != '?') {
-                        $fail('Are you sure that is a question ? It is missing the question mark');
-                    }
-                }],
+                new EndWithQuestionMarkRule(),
+            ],
         ]);
 
         Question::query()->create($attributes);
