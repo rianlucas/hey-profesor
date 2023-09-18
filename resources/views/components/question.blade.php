904c59579da48e2a65241be7e98a82f690a2b151
Rian Lucas <rianluccas08@gmail.com>
2023-09-18 17:57:03 -0300

    hey-9: beign able to see a list of questions in the dasboard

---
+++ resources/views/components/question.blade.php
@@ -0,0 +1,8 @@
+@props([
+    'question'
+])
+
+
+<div class="rounded dark:bg-gray-800/50 shadow-md shadow-blue-500/50 p-3 dark:text-gray-200">
+    {{ $question->question }}
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,5 +13,15 @@
 
         </x-form>
 
+        <hr class="border-gray-700 border-dashed mt-4 mb-4">
+
+        <div class="dark:text-gray-300 font-bold mb-1 uppercase"> List of Questions </div>
+
+        <div class="dark:text-gray-400 space-y-4">
+            @foreach($questions as $item)
+                <x-question :question="$item"></x-question>
+            @endforeach
+        </div>
+
     </x-container>
 </x-app-layout>
