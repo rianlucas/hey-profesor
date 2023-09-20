7c2f4919ac8cfadf387294c2801d137fe4c96324
Rian Lucas <rianluccas08@gmail.com>
2023-09-20 17:34:20 -0300

    hey-9: use updateOrCreate method to like the questions

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -20,3 +20,24 @@ it('Should be able to like a question', function () {
         'user_id'     => $user->id,
     ]);
 });
+
+it('should not be able to more than one time', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->create();
+
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    expect($user->votes()->where('question_id', '=', $question->id)->get())
+        ->toHaveCount(1);
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'like'        => 1,
+        'unlike'      => 0,
+        'user_id'     => $user->id,
+    ]);
+});
