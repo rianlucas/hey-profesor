b6662f63337a311a9672b231a152686322f84eb7
Rian Lucas <rianluccas08@gmail.com>
2023-09-23 13:29:17 -0300

    hey-9: handling N+1 issue on like and unlike queries

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -9,14 +9,14 @@
         <x-form :action="route('question.like', $question)" >
             <button class="flex items-start space-x-2 text-green-500">
                 <x-icons.thumbs-up class="2-5 h-5 hover:text-green-300 cursor-pointer" id="thumbs-up"/>
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
         </x-form>
 
         <x-form :action="route('question.unlike', $question)">
             <button class="flex items-start space-x-2 text-red-500">
                 <x-icons.thumbs-down class="2-5 h-5 text-red-500 hover:text-red-300 cursor-pointer" id="thumbs-up"/>
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
         </x-form>
 
