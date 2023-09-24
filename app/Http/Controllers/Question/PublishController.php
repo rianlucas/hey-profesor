3aee5edbedc6e0611bc9a9c7eacc6d244015e9ca
Rian Lucas <rianluccas08@gmail.com>
2023-09-24 13:51:37 -0300

    hey-14: ït should be ablle to commit a question

---
+++ app/Http/Controllers/Question/PublishController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Question;
+
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use Illuminate\Http\{RedirectResponse, Request};
+
+class PublishController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Question $question): RedirectResponse
+    {
+        $question->update(['draft' => false]);
+
+        return back();
+    }
+}
