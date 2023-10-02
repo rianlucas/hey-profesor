7500cd2435ee3b012a006405f812af084169eb80
Rian Lucas <rianluccas08@gmail.com>
2023-10-02 17:22:49 -0300

    hey-16: should make sure that only questions with status DRAFT can be edit

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -16,4 +16,9 @@ class QuestionPolicy
         return $question->createdBy->is($user);
     }
 
+    public function update(User $user, Question $question): bool
+    {
+        return $question->draft;
+    }
+
 }
--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -4,16 +4,44 @@ use App\Models\{Question, User};
 
 use function Pest\Laravel\{actingAs, get};
 
-it('Should be able to open a question to edit', function () {
+it('should be able to open a question to edit', function () {
     $user     = User::factory()->create();
     $question = Question::factory()
         ->for($user, 'createdBy')
         ->create();
-    ;
 
     actingAs($user);
 
     get(route('question.edit', $question))
         ->assertSuccessful();
 
+});
+
+it('should return a view', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => true]);
+
+    actingAs($user);
+
+    get(route('question.edit', $question))
+        ->assertViewIs('question.edit');
+
+});
+
+it('should make sure that only questions with status DRAFT can be edit', function () {
+    $user            = User::factory()->create();
+    $questionNoDraft = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => false]);
+
+    $draftQuestion = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => true]);
+
+    actingAs($user);
+
+    get(route('question.edit', $questionNoDraft))->assertForbidden();
+    get(route('question.edit', $draftQuestion))->assertSuccessful();
 });
