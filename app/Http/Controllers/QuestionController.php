95f0fc51fa6909d8e833fb3f32e131bc6d093073
Rian Lucas <rianluccas08@gmail.com>
2023-10-04 21:07:16 -0300

    hey-16: should make sure that only questions with status DRAFT and only if the question was created by the logged user can be updated

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -51,6 +51,8 @@ class QuestionController extends Controller
 
     public function update(Question $question): RedirectResponse
     {
+        $this->authorize('update', $question);
+
         $question->question = request()->question;
         $question->save();
 
--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -2,13 +2,54 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\{actingAs, put};
+use function Pest\Laravel\{actingAs, get, put};
 
 it('should be able to update a question', function () {
     $user     = User::factory()->create();
     $question = Question::factory()
         ->for($user, 'createdBy')
         ->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.update', $question), [
+        'question' => 'Updated Question?',
+    ])->assertRedirect();
+
+    $question->refresh();
+
+    expect($question->question)->toBe('Updated Question?');
+});
+
+it('should make sure that only questions with status DRAFT can be updated', function () {
+    $user            = User::factory()->create();
+    $questionNoDraft = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => false]);
+
+    $draftQuestion = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.update', $questionNoDraft))->assertForbidden();
+    put(route('question.update', $draftQuestion), ['question' => 'Updated Question ?'])->assertRedirect();
+});
+
+it('should make sure that only the person who has created the question can update the question', function () {
+    $user      = User::factory()->create();
+    $wrongUser = User::factory()->create();
+
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => true]);
+
+    actingAs($wrongUser);
+
+    put(route('question.update', $question), [
+        'question' => 'Updated Question?',
+    ])->assertForbidden();
 
     actingAs($user);
 
